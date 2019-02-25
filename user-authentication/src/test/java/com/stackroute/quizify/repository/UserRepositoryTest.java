70bc12fe837d1274b1f302afa2bc751ef0172570
Jasleen <jasleka1@in.ibm.com>
2019-02-25 14:28:12 +0530

    user-authentication

--- user-authentication/src/test/java/com/stackroute/quizify/repository/UserRepositoryTest.java
+++ user-authentication/src/test/java/com/stackroute/quizify/repository/UserRepositoryTest.java
@@ -1,6 +1,8 @@
-//package com.stackroute.repository;
 //
-//import User;
+//package com.stackroute.quizify.repository;
+//import com.stackroute.quizify.repository.UserRepository;
+//import com.stackroute.quizify.domain.User;
+//import com.stackroute.quizify.repository.UserRepository;
 //import org.junit.After;
 //import org.junit.Assert;
 //import org.junit.Before;
@@ -11,7 +13,7 @@
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 //import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //import org.springframework.test.context.junit4.SpringRunner;
-//import UserRepository;
+//
 //import java.util.List;
 //
 //import static org.junit.Assert.*;
@@ -28,24 +30,31 @@
 //
 //    @Before
 //    public void setUp() {
-////        user = new User();
-////        user.setUsername("ADMIN");
-////        user.setPassword("123");
-////        user.setRole("admin");
-////        userRepository.save(user);
+//        user = new User();
+//        user.setUsername("ADMIN");
+//        user.setPassword("123");
+//        user.setRole("admin");
+//        userRepository.save(user);
 //    }
 //
+////    @Test
+////    public void findByUsernameAndPasswordTestSuccess(){
+////
+////        User user1=userRepository.findById(userRepository.findByUsernameAndPassword("admin","123"));
+////
+////       Assert.assertEquals("admin",user1.getUsername());
+////    }
 //
-//    @Test
-//    public void testSaveUser(){
+////    @Test
+////    public void testSaveUser(){
 ////        userRepository.save(user);
 ////        User fetchUser = userRepository.findById(user.getUsername()).get();
 ////        Assert.assertEquals("ADMIN",fetchUser.getUsername());
+////
+////    }
 //
-//    }
-//
-//    @Test
-//    public void testGetAllTrack(){
+////    @Test
+////    public void testGetAllTrack(){
 ////        User u = new User("jass","123","player");
 ////        User u1 = new User("kaur","789","player");
 ////        userRepository.save(u);
@@ -53,16 +62,16 @@
 ////
 ////        List<User> list = userRepository.findAll();
 ////        Assert.assertEquals("player",list.get(1).getRole());
-//
-//
-//
-//
-//    }
-//    @Test
-//    public void testSaveTrackFailure(){
+////
+////
+////
+////
+////    }
+////    @Test
+////    public void testSaveTrackFailure(){
 ////        User testUser = new User("hello","Hello","hello");
 ////        userRepository.save(user);
 ////        User fetchUser = userRepository.findById(user.getUsername()).get();
 ////        Assert.assertNotEquals(testUser,user);
-//    }
+////    }
 //}
