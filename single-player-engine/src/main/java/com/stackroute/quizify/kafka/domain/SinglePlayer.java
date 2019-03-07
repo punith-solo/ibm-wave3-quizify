2f836149f2852770945e467dc8ab91e3bdbb2367
Rutuja.Bacchuwar <rutuja.bacchuwar@stackroute.in>
2019-03-07 10:37:56 +0530

    single player backend + front end

--- single-player-engine/src/main/java/com/stackroute/quizify/kafka/domain/SinglePlayer.java
+++ single-player-engine/src/main/java/com/stackroute/quizify/kafka/domain/SinglePlayer.java
@@ -5,8 +5,8 @@ import org.springframework.data.annotation.Id;
 
 @Data
 public class SinglePlayer {
-    @Id
-    private long id;
+
+    private String playerId;
     private Game game;
-    private User users;
+
 }
