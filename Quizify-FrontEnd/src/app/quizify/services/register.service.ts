bb4991492dec548c96696e40a8ac25700b0b645c
Jasleen <jasleka1@in.ibm.com>
2019-03-08 12:49:21 +0530

    login front end modified

--- Quizify-FrontEnd/src/app/quizify/services/register.service.ts
+++ Quizify-FrontEnd/src/app/quizify/services/register.service.ts
@@ -6,9 +6,9 @@ import { Register } from '../tsclasses/register';
 })
 export class RegisterService {
 
-   // profile(jti: any): any {
-   //    throw new Error("Method not implemented.");
-   //  }
+   profile(jti: any): any {
+      throw new Error("Method not implemented.");
+    }
 
    url: string;
       topicUrl: string;
