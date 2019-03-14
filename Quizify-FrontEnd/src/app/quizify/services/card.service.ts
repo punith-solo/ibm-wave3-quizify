ff12a39d219ff2e7ee25b85c33090189718a9489
TJayashree1412 <jayashree1412@gmail.com>
2019-03-14 12:23:24 +0530

    pull

--- Quizify-FrontEnd/src/app/quizify/services/card.service.ts
+++ Quizify-FrontEnd/src/app/quizify/services/card.service.ts
@@ -10,7 +10,7 @@ export class CardService {
     }
    getquiz() {
     // this.url = 'http://localhost:3000/quiz';
-    this.url = 'https://13.232.243.68:8104/recommendation-service/rest.neo4j/game/mostplayed';
+    this.url = 'https://13.232.243.68:8104/rest/neo4j/game/mostplayed';
     return this.http.get(this.url);
    }
 
