eed8f43db51ce9c71e1a99ad952d163f0705b717
Kpal22 <kaustavlogan@gmail.com>
2019-03-14 16:14:37 +0530

    edited question and game

---
+++ question-manager-service/src/main/java/com/stackroute/quizify/questionmanager/domain/sparQL/QuestionDetails.java
@@ -0,0 +1,13 @@
+package com.stackroute.quizify.questionmanager.domain.sparQL;
+
+import com.stackroute.quizify.questionmanager.domain.Category;
+import com.stackroute.quizify.questionmanager.domain.Genre;
+import com.stackroute.quizify.questionmanager.domain.Topic;
+import lombok.Data;
+
+@Data
+public class QuestionDetails {
+    private Category category;
+    private Topic topic;
+    private Genre genre;
+}
